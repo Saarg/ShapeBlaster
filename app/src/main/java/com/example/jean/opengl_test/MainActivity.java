6a7209c48ed2aff069e78fb34de373c799617672
BuildTools <milsonneau.jean@free.fr>
2016-10-03 21:03:29 +0200

    First commit, triangle in view

---
+++ app/src/main/java/com/example/jean/opengl_test/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.jean.opengl_test;
+
+import android.app.Activity;
+import android.content.Context;
+import javax.microedition.khronos.egl.EGLConfig;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.os.Bundle;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class MainActivity extends Activity {
+
+    private MyGLSurfaceView mGLView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
+    }
+
+}
+
+class MyGLSurfaceView extends GLSurfaceView {
+
+    private final MyGLRenderer mRenderer;
+
+    public MyGLSurfaceView(Context context){
+        super(context);
+
+        // Create an OpenGL ES 2.0 context
+        setEGLContextClientVersion(2);
+
+        mRenderer = new MyGLRenderer();
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        setRenderer(mRenderer);
+    }
+}
---
+++ app/src/main/java/com/example/jean/opengl_test/MyGLRenderer.java
@@ -0,0 +1,75 @@
+package com.example.jean.opengl_test;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
+import com.example.jean.opengl_test.shapes.Square;
+import com.example.jean.opengl_test.shapes.Triangle;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by jean on 03/10/16.
+ */
+
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+    private Triangle _Triangle;
+    private Square _Square;
+
+    // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+    private final float[] _MVPMatrix = new float[16];
+    private final float[] _ProjectionMatrix = new float[16];
+    private final float[] _ViewMatrix = new float[16];
+
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        // Set the background frame color
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        // initialize a triangle
+        _Triangle = new Triangle();
+        // initialize a square
+        _Square = new Square();
+
+    }
+
+    public void onDrawFrame(GL10 unused) {
+        // Redraw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(_ViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(_MVPMatrix, 0, _ProjectionMatrix, 0, _ViewMatrix, 0);
+
+        // Draw shape
+        _Triangle.draw(_MVPMatrix);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(_ProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+    }
+
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
+    }
+}
